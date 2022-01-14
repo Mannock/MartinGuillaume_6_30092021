17bdec664a32124f12bb7fe1f96490da10ac7c75
Martin Guillaume <pour-le-travail@hotmail.fr>
2022-01-14 13:20:51 +0100

    up mentorat

--- javascript/objects/medias.js
+++ javascript/objects/medias.js
@@ -1,4 +1,4 @@
-class Media {
+export class Media {
   constructor(id, photographerId, title, image, tags, likes, date, price) {
     this.id = id;
     this.photographerId = photographerId;
@@ -10,5 +10,28 @@ class Media {
     this.price = price;
   }
 
-  display() {}
+  //   display(medias) {
+  //     medias.forEach(media => {
+  //       document.querySelector(".work-display").innerHTML = medias
+  //     .map((media) => {
+  //       return `<div class="work-display__card">
+  //         <img
+  //           class="work-display__card__image"
+  //           src="./img/Sample Photos/Mimi/Animals_Rainbow.jpg"
+  //           alt=""
+  //         />
+  //         <div class="infos-card">
+  //           <h2>Arc-en-ciel</h2>
+  //           <div class="counter">
+  //             <p>12</p>
+  //             <img src="./img/heart-icon.png" alt="heart icon" />
+  //           </div>
+  //         </div>
+  //         </div>
+
+  //             `;
+  //     })
+  //     .join("");
+  // };
+  //     }
 }
